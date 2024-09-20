acbf62345bbad8ae7bc80a619074df0dda696c33
laureenbht <146077695+laureenbht@users.noreply.github.com>
2024-09-20 15:14:39 +0200

    [UPDATE] simulateur

--- simulateur.py
+++ simulateur.py
@@ -8,9 +8,10 @@ import lightgbm as lgb
 import streamlit as st
 
 # @st.cache
-df = pd.read_csv('00_testing_clean.csv')
+df = pd.read_csv('merged_all.csv')
 
 
+# Conversion des temps ( 1:30:28 ) en secondes
 def convert_to_seconds(time_str):
     try:
         if pd.isna(time_str) or time_str == '\\N':
@@ -30,42 +31,67 @@ numeric_cols = df.select_dtypes(include=[np.number]).columns
 df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
 df['fastestLapSpeed'] = df['fastestLapSpeed'].astype(float)
 
-
+# Sélection des paramètres et de la valeur
 x = df[['circuitId', 'Humidity', 'Rainfall', 'TrackTemp', 'AirTemp', 'WindDirection', 'WindSpeed', 'driverId', 'time',
         'fastestLapTime', 'HighTemp', 'fastestLapSpeed']]
 y = df[['position']]
 
+# Division des données en jeu d'entraînement et de test
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 scaler = StandardScaler()
 x_train_scaled = scaler.fit_transform(x_train)
 x_test_scaled = scaler.transform(x_test)
 
+# Entrainement du modele ( train et test )
 train_data = lgb.Dataset(x_train_scaled, label=y_train)
 params = {}
 model = lgb.train(params, train_data, num_boost_round=200)
 
-st.title('Simulateur de courses F1')
+# Style de l'interface
+st.title('Simulateur de courses F1 🏁')
+st.markdown("""
+    <style>
+        .main {
+            background-color: #000000;
+            color: white;
+        }
+        .reportview-container {
+            background-color: #000000;
+            color: black;
+        }
+        .css-1q8dd3e {
+            color: white !important;
+        }
+        .stButton>button {
+            background-color: #000000;
+            color: black;
+        }
+    </style>
+    """, unsafe_allow_html=True)
 
 circuits = df['circuitRef'].unique()
-selected_circuit = st.selectbox('Selectionner un circuit', circuits)
+selected_circuit = st.selectbox('🛤️ Sélectionner un circuit', circuits)
 
-temp = st.slider('Température de l\'air (C°)', min_value=int(df['AirTemp'].min()), max_value=int(df['AirTemp'].max()),
+# Choix des paramètres de la course au début
+temp = st.slider('🌡️ Température de l\'air (C°)', min_value=int(df['AirTemp'].min()),
+                 max_value=int(df['AirTemp'].max()),
                  value=int(df['AirTemp'].mean()))
-temp_track = st.slider('Température du sol (C°)', min_value=int(df['TrackTemp'].min()),
+temp_track = st.slider('🌡️ Température du sol (C°)', min_value=int(df['TrackTemp'].min()),
                        max_value=int(df['TrackTemp'].max()), value=int(df['TrackTemp'].mean()))
-humidity = st.slider('Humidité (%)', min_value=int(df['Humidity'].min()), max_value=int(df['Humidity'].max()),
+humidity = st.slider('💧 Humidité (%)', min_value=int(df['Humidity'].min()), max_value=int(df['Humidity'].max()),
                      value=int(df['Humidity'].mean()))
-wind_speed = st.slider('Vitesse du vent', min_value=int(df['WindSpeed'].min()), max_value=int(df['WindSpeed'].max()),
+wind_speed = st.slider('💨 Vitesse du vent', min_value=int(df['WindSpeed'].min()), max_value=int(df['WindSpeed'].max()),
                        value=int(df['WindSpeed'].mean()))
-rain = st.radio('Présence de pluie?', ('Oui', 'Non'))
+rain = st.radio('🌧️ Présence de pluie?', ('Oui', 'Non'))
+
 high_temp = 1 if temp > 30 else 0
 strong_wind = 1 if wind_speed > 20 else 0
 pilotes = df['driverRef'].unique()
-selected_pilotes = st.multiselect('Selectionner les pilotes', pilotes, default=pilotes[:22])
+selected_pilotes = st.multiselect('🏎️ Sélectionner les pilotes', pilotes, default=pilotes[:22])
 
 if len(selected_pilotes) != 22:
-    st.warning('Vous devez sélectionnez exactement 22 pilotes')
+    st.warning('⚠️ Vous devez sélectionnez exactement 22 pilotes.')
 else:
     circuit_encoded = df[df['circuitRef'] == selected_circuit]['circuitId'].iloc[0]
     results = []
@@ -83,34 +109,42 @@ else:
         else:
             input_data_scaled = scaler.transform(input_data)
             position_pred = model.predict(input_data_scaled)[0]
-            results.append((position_pred, driver))  # Correction ici
+            results.append((position_pred, driver))
 
-    # Trier les résultats en fonction de la position prédite (indice 0 du tuple)
     results = sorted(results, key=lambda x: x[0])
 
-    # Construire le classement final
+    # Classement final
     classement_final = []
     for idx, result in enumerate(results):
-        classement_final.append([result[1], idx + 1, result[0]])  # Ajouter le pilote, position estimée et score
+        classement_final.append([result[1], idx + 1, result[0]])
 
-    top_3 = classement_final[:3]  # Extraire les trois premiers résultats
-    st.subheader("Top 3 des gagnants de la course")
+    # Top 3
+    top_3 = classement_final[:3]
+    st.subheader("🏆 Top 3 des gagnants de la course")
     for idx, (driver, _, score) in enumerate(top_3):
-        st.write(f"{idx + 1}. {driver}")  # Utiliser l'index pour le classement
+        st.write(f"**{idx + 1}. {driver}**")
 
+    #Saut de ligne
+    st.write("")
+    st.write("")
+    st.write("")
+
+    # Evaluation du modele
     y_pred = model.predict(x_test_scaled)
     mae = metrics.mean_absolute_error(y_test, y_pred)
     mse = metrics.mean_squared_error(y_test, y_pred)
-    st.write(f"MAE: {mae:.2f}")
-    st.write(f"MSE: {mse:.2f}")
+
+    # Explication des metriques
+    st.subheader('📊 Évaluation du modèle')
+    st.write(f"**MAE**: {mae:.2f}")
+    st.write(
+        "Le **MAE** (Mean Absolute Error) mesure l'erreur moyenne entre les positions prédites et les positions réelles. Plus cette valeur est faible, plus le modèle est précis.\n\n")
+
+    st.write(f"**MSE**: {mse:.2f}")
+    st.write(
+        "Le **MSE** (Mean Squared Error) est similaire au MAE, mais il pénalise davantage les erreurs importantes. Une valeur faible est aussi un bon indicateur de précision.\n\n")
 
     ndcg_val = ndcg_score([y_test.values.flatten()], [y_pred.flatten()], k=10)
-    st.write(f"NDCG@10: {ndcg_val:.2f}")
-
-
-
-
-
-
-
-
+    st.write(f"**NDCG@10**: {ndcg_val:.2f}")
+    st.write(
+        "Le **NDCG@10** (Normalized Discounted Cumulative Gain) mesure la qualité du classement des positions prédites, en tenant compte de leur ordre. Une valeur proche de 1 indique un bon classement.\n")
